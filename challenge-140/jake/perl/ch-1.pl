9b6721536b66d3af526e7c132a089b7dc3a832a5
dasJake <no_mail@github.com>
2021-11-23 20:04:03 +0100

    140 add alex' ideas for traditional approach

--- challenge-140/jake/perl/ch-1.pl
+++ challenge-140/jake/perl/ch-1.pl
@@ -9,32 +9,116 @@ use warnings;
 print "decimal number a: ";
 my $a = <STDIN>;
 chomp $a;
-my $power_n = 0;
-my $a_binary += decimal_to_binary ( $power_n, $a );
-printf ( "binary number a: %i\n", $a_binary );
+my $power_n  = 0;
+my $a_binary = 0;
+$a_binary += decimal_to_binary( $power_n, $a, $a_binary );
+printf( "binary number a: %i\n", $a_binary );
+printf( "binary number b: %i\n", dec_to_bin($a) );
+printf( "binary number b: %i\n", dec_to_bin_end_rec($a) );
 
 # convert dec to bin
 sub decimal_to_binary {
-    my ( $power, $decimal_number ) = @_;
+    my ( $power, $decimal_number, $binary_quantum ) = @_;
+
     if ( $decimal_number == 0 ) {
         return 0;
-        #exit;
+    }
+
+    if ( $decimal_number == 2**$power ) {
+        $binary_quantum += 10**$power;
+        return $binary_quantum;
+    }
+
+    if ( $decimal_number > 2**$power && $decimal_number < 2**( $power + 1 ) ) {
+        print "if\n$decimal_number\n$power\n\n";
+        $binary_quantum += 10**$power;
+        $decimal_number -= 2**($power);
+        $power = 0;
+        return decimal_to_binary( $power, $decimal_number, $binary_quantum );
+    }
+
+    $power++;
+    return decimal_to_binary( $power, $decimal_number, $binary_quantum );
+}
+
+sub dec_to_bin {
+    my ($n) = @_;
+
+    my $acc = '';
+    while ( $n >= 1 ) {
+        my $rem = $n % 2;
+        $n   = int( $n / 2 );
+        $acc = $rem . $acc;
+    }
+    return $acc;
+}
+
+sub dec_to_bin_end_rec {
+    my ($n) = @_;
+
+    return _dec_to_bin_end_rec( $n, '' ) || 0;
+}
+
+sub _dec_to_bin_end_rec {
+    my ( $n, $acc ) = @_;
+
+    if ( $n == 0 ) {
+        return $acc;
+    }
+
+    my $next = int( $n / 2 );
+    my $rem  = $n % 2;
+
+    return _dec_to_bin_end_rec( $next, $rem . $acc );
+}
+
+sub dec_to_oct {
+    my ($n) = @_;
+
+    if ( $n <= 1 ) {
+        return $n;
+    }
+
+    my $next = int( $n / 16 );
+    my $rem  = $n % 16;
+
+
+    return dec_to_bin($next) . $rem;
+}
+
+sub dec_to_bin_rec {
+    my ($n) = @_;
+
+    if ( $n <= 1 ) {
+        return $n;
+    }
+
+    my $next = int( $n / 2 );
+    my $rem  = $n % 2;
+
+    return dec_to_bin($next) . $rem;
+}
+
+sub _decimal_to_binary {
+    my ( $power, $decimal_number, $binary_quantum ) = @_;
+    if ( $decimal_number == 0 ) {
+        return 0;
     }
     elsif ( $decimal_number == 2**$power ) {
         print "if\n$decimal_number\n$power\n\n";
-        return 10**$power;
-        #exit;
+        $binary_quantum *= 10**$power;
+        return $binary_quantum;
     }
-    elsif ( $decimal_number <= 2**($power+1) ) {
+    elsif ( $decimal_number <= 2**( $power + 1 ) ) {
         $power++;
-        decimal_to_binary ( $power, $decimal_number );
         print "elsif\n$decimal_number\n$power\n\n";
+        _decimal_to_binary( $power, $decimal_number, $binary_quantum );
     }
     else {
-        return 10**($power+1);
-        $decimal_number -= 2**($power+1);
+        $binary_quantum += 10**( $power + 1 );
+        $decimal_number -= 2**( $power + 1 );
         $power = 0;
         print "else\n$decimal_number\n$power\n\n";
-        decimal_to_binary ( $power, $decimal_number );
+        _decimal_to_binary( $power, $decimal_number, $binary_quantum );
     }
 }
